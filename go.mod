b2daef086bcbd9c1cc8e91990b37745c97925553
Adityawarman Fanaro <reznov53@live.com>
2019-03-31 20:15:26 +0700

    Added RabbitMQ publish setup and gitignore

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/reznov53/law-go-assignment3-upload-compress-async
 
 go 1.12
+
+require (
+	github.com/streadway/amqp v0.0.0-20190312223743-14f78b41ce6d
+	golang.org/x/net v0.0.0-20190328230028-74de082e2cca
+)
--- server/mq.go
+++ server/mq.go
@@ -1,1 +1,48 @@
-package server
+package main
+
+import (
+	"log"
+	"os"
+
+    "github.com/streadway/amqp"
+)
+
+var ch *amqp.Channel
+var err error
+var conn *amqp.Connection
+
+func failOnError(err error, msg string) {
+    if err != nil {
+        log.Fatalf("%s: %s", msg, err)
+    }
+}
+
+func init() {
+	url := os.Getenv("URL")
+	vhost := os.Getenv("VHOST")
+	exchangeName := os.Getenv("EXCNAME")
+	exchangeType := os.Getenv("EXCTYPE")
+
+	conn, err = amqp.Dial(url + vhost)
+    failOnError(err, "Failed to connect to RabbitMQ")
+    defer conn.Close()
+
+    ch, err = conn.Channel()
+    failOnError(err, "Failed to open a channel")
+    defer ch.Close()
+
+    err = ch.ExchangeDeclare(exchangeName, exchangeType, false, false, false, false, nil)
+    failOnError(err, "Failed to declare exchange")
+}
+
+func main() {
+
+	body := "Test"
+	
+	err = ch.Publish("exchange_ping", "", false, false, amqp.Publishing{
+		ContentType:	"text/plain",
+		Body:			[]byte(body),
+	})
+	failOnError(err, "Failed to publish message")
+	log.Print("sent")
+}
